58e4929e9e031f7b00288cc5f9dd8d972ccb270d
solpark16 <79960346+solpark16@users.noreply.github.com>
2022-02-22 18:17:15 +0900

    meetLG_B안_1차최종

--- meetLG/meetLGB/js/script.js
+++ meetLG/meetLGB/js/script.js
@@ -6,15 +6,17 @@ $(function(){
       var headerGnb = vw * 375 / 1920;
       var headerWidth = vw * 110 / 1920;
 	$firstMenu.mouseenter(function(){
-		$header.stop().animate({height:headerGnb});
+		$header.addClass('active');
     $('.backGround').css({
-      'display':'block'
+      'display':'block',
+      'opacity':'40%'
     })
 	});
 	$header.mouseleave(function(){
-		$header.stop().animate({height:headerWidth});
+		$header.removeClass('active');
     $('.backGround').css({
-      'display':'none'
+      'display':'none',
+      'opacity':'0'
     });
 	});
 });
@@ -101,6 +103,28 @@ $('.videoBtnLife').mouseleave(function(){
     })
 })
 
+// gif3 Hover
+$('.gif3').mouseenter(function(){
+  $('.gif3Hover').css({
+      'display' : 'block'
+  })
+})
+$('.gif3').mouseleave(function(){
+  $('.gif3Hover').css({
+      'display' : 'none'
+  })
+})
+
+// section3 apply button
+// 버튼에 마우스 오버 시
+// p에 지원하기 텍스트 추가
+$('.apply').mouseover(function(){
+  $('.apply p').html('지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기 지원하기');
+});
+$('.apply').mouseleave(function(){
+  $('.apply p').html('지원하기');
+});
+
 // section4 List
 $('.interviewLi1').click(function(){
   console.log('click')
@@ -152,15 +176,15 @@ var scene = new ScrollMagic.Scene({
 .addTo(controller);
 
 // csr 둥둥
-// var ani2_png3 = new TimelineMax();
-// ani2_png3.to($('.section5 .sec2Back2'), 1, {opacity: 1, delay: 1.5, animationName: 'pngAni2'})
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: '.section2',
-//   triggerHook: 0.8
-// })
-// .reverse(false)
-// .setTween(ani2_png3)
-// .addTo(controller);
+var ani2_png3 = new TimelineMax();
+ani2_png3.to($('.section5 .csrPerson'), 1, {opacity: 1, delay: 1, animationName: 'pngAni3'})
+var scene = new ScrollMagic.Scene({
+  triggerElement: '.section5',
+  triggerHook: 0.8
+})
+.reverse(false)
+.setTween(ani2_png3)
+.addTo(controller);
 
 // 생기면서 위로 올라오는 애니메이션
 
@@ -195,26 +219,6 @@ var scene = new ScrollMagic.Scene({
   .setTween(sec23)
   .addTo(controller);
 
-// var sec24 = new TimelineMax();
-// sec24.to($('.section2 .sec2Back1'), 1, {opacity: 1, delay: 1.5, top: 0})
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: '.section2',
-//   triggerHook: 0.9
-// })
-//   .reverse(false)
-//   .setTween(sec24)
-//   .addTo(controller);
-
-// var sec25 = new TimelineMax();
-// sec25.to($('.section2 .sec2Back2'), 1, {opacity: 1, delay: 3, top: 0})
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: '.section2',
-//   triggerHook: 0.9
-// })
-//   .reverse(false)
-//   .setTween(sec25)
-//   .addTo(controller);
-
 // section3 animation
 var sec31 = new TimelineMax();
 sec31.to($('.section3 .section3Text'), 1, {opacity: 1, delay: 0.6, top: 0})
@@ -227,7 +231,7 @@ var scene = new ScrollMagic.Scene({
 .addTo(controller);
 
 var sec32 = new TimelineMax();
-sec32.to($('.section3 .applyBtn'), 1, {opacity: 1, delay: 0.6, top: 0})
+sec32.to($('.section3 .apply'), 1, {opacity: 1, delay: 0.6, top: 0})
 var scene = new ScrollMagic.Scene({
   triggerElement: '.section3',
   triggerHook: 0.9
@@ -278,12 +282,12 @@ var scene = new ScrollMagic.Scene({
 .setTween(sec51)
 .addTo(controller);
 
-var sec52 = new TimelineMax();
-sec52.to($('.section5 .newsArea'), 1, {opacity: 1, delay: 3, top: 0})
+var sec61 = new TimelineMax();
+sec61.to($('.section6 .newsArea'), 1, {opacity: 1, delay: 0.6, top: 0})
 var scene = new ScrollMagic.Scene({
-  triggerElement: '.section5',
+  triggerElement: '.section6',
   triggerHook: 0.9
 })
 .reverse(false)
-.setTween(sec52)
+.setTween(sec61)
 .addTo(controller);
