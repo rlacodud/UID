c9ac96ac69eb1987a89a2abe50c975a17f8665d5
rlacodud <74367207+rlacodud@users.noreply.github.com>
2022-03-03 14:50:38 +0900

    포폴_토레타_수정

--- portfolio/chaeyoung_portfolio/Toreta!/js/bubble.js
+++ portfolio/chaeyoung_portfolio/Toreta!/js/bubble.js
@@ -1,46 +1,34 @@
-/**
- * Utilities static function
- */
  class Utils {
     /**
-     * Get random number
-     * 원이 랜덤으로 생성되도록 최소값과 최대값을 변수로 설정해줍니다.
      * @param {number} min - min number
      * @param {number} max - max number
      */
-    // 변수를 이용하여 랜덤한 범위를 생성합니다.
     static getRandomNumber(min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min);
     }
     /**
-     * Get rgb color if not params return random
      * @param {number} r - red
      * @param {number} g - green
      * @param {number} b - blue
      */
     static getRGBColor(r, g, b) {
-      // 흰색을 설정하기 위해 지정해줍니다.
       const rr = r || this.getRandomNumber(0, 255);
       const rg = g || this.getRandomNumber(0, 255);
       const rb = b || this.getRandomNumber(0, 255);
       return 'rgb(' + rr + ', ' + rg + ', ' + rb + ')';
     }
     /**
-     * Get hsl color if not params return random
      * @param {number} hue - from 0 to 360
      * @param {number} saturation - from 0 to 100
      * @param {number} lightness - from 0 to 100
      */
-    // 채도, 색조, 명도를 설정해줍니다.
     static getHSLColor(hue, saturation, lightness) {
       const rHue = hue || this.getRandomNumber(0, 360);
       const rSaturation = saturation || this.getRandomNumber(0, 100);
       const rLightness = lightness || this.getRandomNumber(0, 100);
-      // 각각 0~360 사이, 0~100 사이의 값 안에서 랜덤으로 값을 대입받아 반환받습니다.
       return 'hsl(' + rHue + ', ' + rSaturation + '%, ' + rLightness + '%)';
     }
     /**
-     * Get gradient color
      * @param {ctx} ctx - context for canvas
      * @param {string} type - linear or radial
      * @param {number} r - red from 0 to 255
@@ -53,11 +41,9 @@
      * @param {number} x - end coordinate x
      * @param {number} y - end coordinate y
      */
-    // 각 그라디언트에 적합한 조합을 매개 변수로 삼아
     static getGradientColor(ctx, type, cr, cg, cb, ca, x, y, r, ex, ey) {
       let col, g;
       switch (type) {
-        // type이 linear일 경우
         case 'linear':
           col = cr + "," + cg + "," + cb;
           g = ctx.createLinearGradient(x, y, ex, ey);
@@ -67,25 +53,19 @@
           return g;
           break;
         case 'radial':
-          // type이 radial일 경우
           col = cr + "," + cg + "," + cb;
           g = ctx.createRadialGradient(x, y, 0, x, y, r);
           g.addColorStop(0, "rgba(" + col + ", " + (ca * 1) + ")");
           g.addColorStop(0.5, "rgba(" + col + ", " + (ca * 0.5) + ")");
           g.addColorStop(1, "rgba(" + col + ", " + (ca * 0) + ")");
           return g;
-          // 각각의 식으로 계산 후 탈출합니다.(반환)
           break;
-          // 둘 다 아닐 경우,
         default:
-          // mistaken params를 출력 후
-          console.log('mistaken params');
-          // 탈출합니다.(반환)
+          // console.log('mistaken params');
           break;
       } 
     }
     /**
-     * Get multiple array
      * @param {number} yLength - length
      * @param {number} xLength - length
      */
@@ -101,27 +81,12 @@
     }
   }
   
-  /**
-   * Vector
-   * Referenced / O'Reilly Programming HTML5 Canvas
-   */
   class Vector2d {
-    /*
-    * @constructor
-    * @param {number} x - vector x
-    * @param {number} y - vector y
-    */
     constructor(x, y) {
       this.x = x;
       this.y = y;
     }
   
-    /*
-    * Add vector or return new vector
-    * @param {constructor} v - vector or null
-    * @param {number} x - vector x
-    * @param {number} y - vector y
-    */
     add(v, x, y) {
       if (v instanceof Vector2d) {
         return new Vector2d(this.x + v.x, this.y + v.y);
@@ -130,13 +95,6 @@
         this.y += y;
       }
     }
-    
-    /*
-    * Subtract vector or return new vector
-    * @param {constructor} v - vector or null
-    * @param {number} x - vector x
-    * @param {number} y - vector y
-    */
     sub(v, x, y) {
       if (v instanceof Vector2d) {
         return new Vector2d(this.x - v.x, this.y - v.y);
@@ -145,12 +103,6 @@
         this.y -= y;
       }
     }
-    
-    /*
-    * Multiple vector or return new vector
-    * @param {constructor} v - vector or null
-    * @param {number} num
-    */
     mult(v, num) {
       if (v instanceof Vector2d) {
         return new Vector2d(this.x * v.x, this.y * v.x);
@@ -159,28 +111,14 @@
         this.y *= num;
       }
     }
-    
-    /*
-    * Change vector from radian
-    * @param {number} radian
-    */
     fromAngle(radian) {
       this.x = Math.cos(radian);
       this.y = Math.sin(radian);
     }
-  
-    /*
-    * Invert vector
-    */
     negate() {
       this.x = -this.x;
       this.y = -this.y;
     }
-    
-    /*
-    * Change vector from radian
-    * @param {number} radian
-    */
     rotate(radian) {
       const x = this.x;
       const y = this.y;
@@ -189,24 +127,12 @@
       this.x = x * cosVal - y * sinVal;
       this.y = x * sinVal + y * cosVal;
     }
-  
-    /*
-    * Return vector length
-    */
     length() {
       return Math.sqrt(this.x * this.x + this.y * this.y);
     }
-  
-    /*
-    * Return vector length squared
-    */
     lengthSquared() {
       return this.x * this.x + this.y * this.y;
     }
-  
-    /*
-    * Return making vector same direction and noamlize
-    */
     normalize() {
       const len = Math.sqrt(this.x * this.x + this.y * this.y);
       if (len) {
@@ -215,61 +141,32 @@
       }
       return len;
     }
-    
-    /*
-    * Return dot product of vectors
-    * @param {constructor} v - vector
-    */
     dotProduct(v) {
       return this.x * v.x + this.y * v.y;
-    }
-    
+    }  
     edge(v) {
       return this.sub(v);
     }
-    
-    /*
-    * Return vertical vector 
-    */
     perpendicular() {
       const v = new Vector2d();
       v.x = this.y;
       v.y = 0 - this.x;
       return v;
     }
-    
-    /*
-    * Return normalized vertical vector 
-    */
     normal() {
       const p = this.perpendicular();
       return p.normalize();
     }
-  
-    /*
-    * Display vector by string 
-    */
     toString() {
       return '(' + this.x.toFixed(3) + ',' + this.y.toFixed(3) + ')';
     }
   }
-  
-  /**
-   * Collide
-   * Referenced / O'Reilly Programming HTML5 Canvas
-   */
   class CollideWithCircle {
-    /*
-    * @constructor
-    * @param {array} array - target array
-    */
+
     constructor(array) {
       this.array = array;
     }
   
-    /*
-    * Check for collide
-    */
     collide() {
       const v = new Vector2d(0, 0);
       let dist, obj1, obj2;
@@ -292,11 +189,6 @@
       }
     }
   
-    /*
-    * Change vector
-    * @param {obj} obj1 - target object
-    * @param {obj} obj2 - target object
-    */
     bounce(obj1, obj2) {
       const colnAngle = Math.atan2(obj1.y - obj2.y, obj1.x - obj2.x);
       const length1 = obj1.v.length();
@@ -314,14 +206,7 @@
     }
   }
   
-  /**
-   * Stop watch
-   * Referenced / O'Reilly Programming HTML5 Canvas
-   */
   class Stopwatch {
-    /*
-    * @constructor
-    */
     constructor() {
       this.startTime = null;
       this.elapsedTime = null;
@@ -330,37 +215,21 @@
       this.fps = null;
       this.lastTime = null;
     }
-    
-    /*
-    * Calculate times
-    */
     calcTime() {
       const time = Date.now();
       this.elapsedTimeForCalc = time - this.lastTime;
       this.fps = 1000 / this.elapsedTimeForCalc;
       this.lastTime = time;
     }
-  
-    /*
-    * Start stop watch
-    */
     start() {
       this.startTime = Date.now();
       this.elapsedTime = null;
       this.running = true;
     }
-  
-    /*
-    * Stop stop watch
-    */
     stop() {
       this.elapsedTime = Date.now() - this.startTime;
       this.running = false;
     }
-  
-    /*
-    * Get elapsed time
-    */
     getElapsedTime() {
       if (this.running) {
         return Date.now() - this.startTime;
@@ -368,26 +237,13 @@
         return this.elapsedTime;
       }
     }
-  
-    /*
-    * Return running
-    */
     isRunning() {
       return this.running;
     }
-  
-    /*
-    * Reset elapsed time
-    */
     reset() {
       this.elapsedTime = null;
     }
   }
-  
-  /**
-   * Animation timer
-   * Referenced / O'Reilly Programming HTML5 Canvas
-   */
   class AnimationTimer {
     constructor(duration, timeWarp, option) {
       if (duration !== undefined) this.duration = duration;
@@ -396,9 +252,7 @@
     }
     
     setTimeWarpFunction(timeWarp, option) {
-      // timeWarp이 정의되지 않았거나 랜덤일 때
       if (timeWarp === undefined || timeWarp === 'Random') {
-        // 배열로 각 timeWarp 대입
         const arr = [
           'EaseIn',
           'EaseOut',
@@ -411,33 +265,24 @@
       }
       switch (timeWarp) {
         case 'EaseIn':
-          //timeWarp이 EaseIn일 때
           return this.makeEaseIn(option);
         case 'EaseOut':
-          //timeWarp이 EaseOut일 때
           return this.makeEaseOut(option);
         case 'EaseInOut':
-          //timeWarp이 EaseInOut일 때
           return this.makeEaseInOut();
         case 'Elastic':
-          //timeWarp이 Elastic일 때
           return this.makeElastic(option);
         case 'Bounce':
-          //timeWarp이 Bounce일 때
           return this.makeBounce(option);
         case 'Linear':
-          //timeWarp이 Linear일 때
           return this.makeLinear();
         default:
-          //timeWarp이 그 무엇도 아닐 때
           return this.makeLinear();
       }
     }
-    // 시작하게 하는 함수
     start() {
       this.stopwatch.start();
     }
-    // 멈추게 하는 함수
     stop() {
       this.stopwatch.stop();
     }
@@ -450,11 +295,9 @@
       return [elapsedTime * (this.timeWarp(percentComplete) / percentComplete), this.timeWarp(percentComplete)];
     }
   
-    // 작동 중일 때의 함수
     isRunning() {
       return this.stopwatch.running;
     }
-    // 작동이 끝났을 때의 함수
     isOver() {
       return this.stopwatch.getElapsedTime() > this.duration;
     }
@@ -501,76 +344,39 @@
       }
     }
   }
-  
-  /**
-   * Angle
-   */
   class Angle {
-    /*
-    * @constructor
-    * @param {number} angle
-    */
     constructor(angle) {
       this.a = angle;
       this.radian = this.a * Math.PI / 180;
     }
-    
-    /*
-    * Return angle
-    */
     getAngle() {
       return this.a;
     }
-    
-    /*
-    * Return radian
-    */
     getRadian() {
       return this.radian;
     }
-    
-    /*
-    * Increase angle
-    */
     increaseAngle(num) {
       this.a += num;
     }
   }
-  
-  /**
-   * Canvas
-   */
   class Canvas {
-    /*
-    * @constructor
-    * @param {bool} bool - true or false
-    */
     constructor(bool) {
-      // create canvas.
-      // canvas를 생성한 뒤
       this.canvas = document.createElement("canvas");
-      // if on screen.
       if (bool === true) {
-        // canvas에 대한 기본 css를 설정합니다.
         this.canvas.style.position = 'relative';
         this.canvas.style.display = 'block';
         this.canvas.style.zIndex = '10';
-        // this.canvas.style.backgroundColor = 'black';
         this.canvas.style.top = '0';
         this.canvas.style.left = '0';
-        // section5에 해당 캔버스를 삽입합니다.
         document.getElementById("section5").appendChild(this.canvas);
       }
       this.ctx = this.canvas.getContext("2d");
       this.width = this.canvas.width = window.innerWidth;
       this.height = this.canvas.height = window.innerHeight;
-      // times
       this.times = null;
-      // mouse infomation.
       this.mouseX = null;
       this.mouseY = null;
       this.mouseZ = null;
-      // shape
       this.numberOfShapes = null;
       this.shapesArray = null;
     }
@@ -586,17 +392,12 @@
     }
   
     render() {
-      // times
       this.times.calcTime();
-      // clear canvas
       this.ctx.clearRect(0, 0, this.width, this.height);
-      // render
       for (let i = 0; i < this.shapesArray.length; i++) {
         this.shapesArray[i].render(Date.now());
       }
-      // draw fps
       this.drawFPS();
-      // itereation
       requestAnimationFrame(() => {
         this.render();
       });
@@ -619,10 +420,6 @@
       this.init();
     }
   }
-  
-  /**
-   * Shape class.
-   */
   class Shape {
     constructor(ctx, ww, wh, index) {
       this.ctx = ctx;
@@ -633,24 +430,18 @@
     
     init(index) {
       this.index = index;
-      // Coordinate
       this.x = this.ww * Math.random();
       this.y = this.wh + 15;
-      // vector
       this.v = new Vector2d(0, -Math.random());
-      // for animation
       this.AnimationTimer = new AnimationTimer(
         Utils.getRandomNumber(500, 5000),
         document.getElementsByTagName('select')[0].value,
         document.getElementsByTagName('input')[0].value
       );
-      // AnimationTimer를 시작시킵니다.
       this.AnimationTimer.start();
       this.lastTime = null;
-      // shape settings
       this.lineWidth = 1;
       this.radius = Math.random() * 10 + 5;
-      // 원의 배경색과 선색을 흰색으로 설정합니다.
       this.fillColor = 'white';
       this.strokeColor = 'white';
       this.globalAlpha = 1;
@@ -664,8 +455,6 @@
       ctx.lineWidth = this.lineWidth;
       ctx.fillStyle = this.fillColor;
       ctx.strokeStyle = this.strokeColor;
-      // 값을 받아와 대입한 뒤
-      // 이를 기반으로 그려 나가도록 합니다.
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
       ctx.fill();
@@ -681,9 +470,7 @@
       this.globalAlpha = 1 - percentComplete;
     }
     
-    // 애니메이션이 작동 중인지 끝났는지를 판단하는 함수
     judgeAnimation() {
-      // 만약 현재 이 애니메이터가 작동 중이라면
       if (this.AnimationTimer.isRunning()) {
         const times = this.AnimationTimer.getElapsedTime();
         const animationElapsed = times[0] || 0.01;
@@ -692,11 +479,8 @@
           let elapsed = animationElapsed - this.lastTime;
           this.updatePositionTimeBased(elapsed / 1000, percentComplete);
         }
-        // 만약 현재 이 애니메이터가 작동이 끝났다면
         if (this.AnimationTimer.isOver()) {
-          // AnimationTimer는 작동을 중지시키고
           this.AnimationTimer.stop();
-          // 초기화 함수를 실행시킵니다.
           this.init(this.index);
           return;
         }
@@ -709,23 +493,14 @@
       this.judgeAnimation();
     }
   }
-  
-  /**
-   * run
-   */
   (() => {
     'use strict';
-    // 윈도우가 로드되었을 때,
-    // 아래의 기능을 작동시킵니다.
     window.addEventListener('load', () => {
-    //   console.clear();
-    // 새 캔버스를 불러와
-    // 캔버스에 init 함수와 render 함수를 작동시킵니다.
+
       const canvas = new Canvas(true);
       canvas.init();
       canvas.render();
       
-      // resize
       window.addEventListener("resize", () => {
         canvas.resize();
       }, false);
--- portfolio/chaeyoung_portfolio/Toreta!/js/gallery.js
+++ portfolio/chaeyoung_portfolio/Toreta!/js/gallery.js
@@ -1,130 +1,96 @@
-// 이미지 갤러리를 감싸고 있는 gallery를 찾습니다.
-var galleryEl = document.getElementById('gallery');
-// 그 안에 있는 콘텐츠들을 감싸는 view를 찾고
-var viewEl = galleryEl.querySelector('.view');
-// 뷰 전체를 감싸는 view-container를 찾습니다.
-var viewContainerEl = viewEl.querySelector('.view-container');
-// view-container 안에 있는 콘텐츠 각각인 view-item을 찾고
-var viewItemEls = viewContainerEl.querySelectorAll('.view-item');
-// 콘텐츠 각각의 이미지를 찾아줍니다.
-var viewItemImgEls = viewContainerEl.querySelectorAll('.view-item > img');
+window.addEventListener('load', convert);
+window.addEventListener('resize', convert);
 
-// 버튼인 list를 찾아주고
-var listEl = galleryEl.querySelector('ul.list');
-// 버튼 각각인 LI를 찾아줍니다.
-var listItemEls = listEl.querySelectorAll('li'); // [li, li, li, li]
-// 그 중 링크 태그가 되어 있는 A까지 찾아내어 활성화 여부를 만들어줄 준비를 합니다.
-var btnListItemEls = listEl.querySelectorAll('li > a'); // [a, a, a, a]
+    function convert() {
+            var convertPx = {
+                vw: function(px){
+                    px = parseFloat(px);
+                    var ww = $(window).width();
+                    
+                    return ww * px / 1920;
+                }
+            }
+    var galleryEl = document.getElementById('gallery');
+    var viewEl = galleryEl.querySelector('.view');
+    var viewContainerEl = viewEl.querySelector('.view-container');
+    var viewItemEls = viewContainerEl.querySelectorAll('.view-item');
+    var viewItemImgEls = viewContainerEl.querySelectorAll('.view-item > img');
 
-// 이미지 갤러리 한 칸의 크기는 800으로 설정하여 너비는 선언해줍니다.
-var _galleryWidth = 800;
-// 콘텐츠 항목의 개수를 length를 통해 찾아줍니다.
-var _galleryMax = viewItemEls.length;
+    var listEl = galleryEl.querySelector('ul.list');
+    var listItemEls = listEl.querySelectorAll('li');
+    var btnListItemEls = listEl.querySelectorAll('li > a');
 
-// 애니메이션과 관련된 수치를 선언해주고
-var _duration = 300;
-var _addDuration = 100;
+    let _galleryWidth = convertPx.vw(600);;
+    var _galleryMax = viewItemEls.length;
 
-// 이전값과 현재값을 선언하며 0으로 초기화해줍니다.
-var cuId = 0;
-var exId = cuId;
+    var _duration = 300;
+    var _addDuration = 100;
 
-// 찾은 LI를 배열로 변환시켜주고
-btnListItemEls = Array.prototype.slice.call(btnListItemEls);
+    var cuId = 0;
+    var exId = cuId;
 
-// 기능이 끝났는지 확인하는 기능을 만들어줍니다.
-// 끝났을 경우, transition속성을 제거하여 완전하게 기능을 끝내도록 합니다.
-function onTransitionEnd() {
-    console.log('end!');
-    viewContainerEl.style.removeProperty('transition');
-}
+    btnListItemEls = Array.prototype.slice.call(btnListItemEls);
 
-// LI 버튼을 눌렀을 때
-function onClickBtnListItem(idx, e) {
-    e.preventDefault();
-    // 현재 눌린 값을 el에 대입하고
-    var el = e.currentTarget;
-    // 현재 선택된 요소의 부모 요소를 itemEl에 대입합니다.
-    var itemEl = el.parentElement;
-    // 이전값이 현재 클릭한 값이 아닐 때
-    if (exId !== idx) {
-        // 현재값에 현재 클릭한 값을 대입하고
-        cuId = idx;
-        // LI에서 이전값의 활성화 클래스를 제거해주고
-        listItemEls[exId].classList.remove('selected');
-        // 현재 눌린 LI에 활성화 클래스를 추가해준 뒤
-        itemEl.classList.add('selected');
-        // 이미지를 슬라이드시키는 함수를 호출합니다.
-        slideGallery();
-    }
-}
-
-// 이미지를 슬라이드시키는 함수를 생성합니다.
-function slideGallery(static) {
-    var bool = static ? static : false;
-    // 콘텐츠의 길이 * 현재 값 * -1을 통해 각 콘텐츠에 따라 다르게 X값을 변형시킬 수 있는 식을 만듭니다.
-    var x = _galleryWidth * cuId * -1;
-    // 그 후 해당 값을 대입하여 컨테이너 내의 위치값을 변형시킵니다.
-    viewContainerEl.style.transform = 'translate3d(' + x + 'px, 0, 0)';
-    // BOOL이 거짓이라면
-    if (!bool) {
-        // MATH 함수를 이용해서 transition의 식을 변형해줍니다.
-        var duration = _duration + _addDuration * Math.abs(exId - cuId); // 400, 500, 600
-        // easing 또한 변수로 제작하여 transition 식에 대입하기 쉽도록 만듭니다.
-        var easing = 'cubic-bezier(0.455, 0.030, 0.515, 0.955)';
-        // 최종적으로 transition 식에 몇초동안 어떤 방식으로 transform될 지에 대한 식을 완성해줍니다.
-        viewContainerEl.style.transition = 'transform ' + duration + 'ms ' + easing;    
-        // 그 후 이전 값에 현재값을 대입하여 다음 클릭 시에도 원활하게 진행될 수 있도록 해줍니다.
-        exId = cuId;
-    } else {
-        // 그게 아닌 경우에는
-        // transition 속성을 제거해줌으로써 transition 기능을 끝냅니다.
+    function onTransitionEnd() {
+        // console.log('end!');
         viewContainerEl.style.removeProperty('transition');
     }
-}
 
-function resizeGallery() {
-
-    // 갤러리의 너비 * 갤러리의 개수로 컨테이너의 총 길이를 변수로 지정해줍니다.
-    var containerWidth = _galleryWidth * _galleryMax;
-    // 갤러리의 너비를 보여지는 콘텐츠의 너비값에 대입시키고
-    viewEl.style.width = _galleryWidth + 'px';
-    // 총 길이를 컨테이너의 너비에 대입시켜줍니다.
-    viewContainerEl.style.width = containerWidth + 'px';
-    // 반복문을 통해 콘텐츠가 변경될 때마다 재정비해줍니다.
-    for(var i = 0; i < _galleryMax; i++) {
-        viewItemEls[i].style.width = _galleryWidth + 'px';
+    function onClickBtnListItem(idx, e) {
+        e.preventDefault();
+        var el = e.currentTarget;
+        var itemEl = el.parentElement;
+        if (exId !== idx) {
+            cuId = idx;
+            listItemEls[exId].classList.remove('selected');
+            itemEl.classList.add('selected');
+            slideGallery();
+        }
     }
-    // 그 후 이미지가 슬라이드시키는 함수를 true로 호출하여 작동하도록 해줍니다.
-    slideGallery(true);
-}
 
-function addEvent() {
-    // 이미지 슬라이드 기능이 끝날 때마다 컨테이너에 onTransitionEnd을 실행시켜줍니다.
-    viewContainerEl.addEventListener('webkitTransitionend', onTransitionEnd);
-    viewContainerEl.addEventListener('transitionend', onTransitionEnd);
-    // 반복문을 통해 li 버튼을 누를 때마다 이미지 슬라이드 기능이 호출되도록 설정해줍니다.
-    for(var i = 0; i < btnListItemEls.length; i++) {
-        btnListItemEls[i].addEventListener('click', onClickBtnListItem.bind(null, i));
+    function slideGallery(static) {
+        var bool = static ? static : false;
+        var x = _galleryWidth * cuId * -1;
+        viewContainerEl.style.transform = 'translate3d(' + x + 'px, 0, 0)';
+        if (!bool) {
+            var duration = _duration + _addDuration * Math.abs(exId - cuId);
+            var easing = 'cubic-bezier(0.455, 0.030, 0.515, 0.955)';
+            viewContainerEl.style.transition = 'transform ' + duration + 'ms ' + easing;    
+            exId = cuId;
+        } else {
+            viewContainerEl.style.removeProperty('transition');
+        }
     }
+
+    function resizeGallery() {
+
+        var containerWidth = _galleryWidth * _galleryMax;
+        viewEl.style.width = _galleryWidth + 'px';
+        viewContainerEl.style.width = containerWidth + 'px';
+        for(var i = 0; i < _galleryMax; i++) {
+            viewItemEls[i].style.width = _galleryWidth + 'px';
+        }
+        slideGallery(true);
+    }
+
+    function addEvent() {
+        viewContainerEl.addEventListener('webkitTransitionend', onTransitionEnd);
+        viewContainerEl.addEventListener('transitionend', onTransitionEnd);
+        for(var i = 0; i < btnListItemEls.length; i++) {
+            btnListItemEls[i].addEventListener('click', onClickBtnListItem.bind(null, i));
+        }
+    }
+    function reset() {
+        cuId = 2;
+        listItemEls[exId].classList.remove('selected');
+        listItemEls[cuId].classList.add('selected');
+        resizeGallery();
+        exId = cuId;
+    }
+
+    function init() {
+        reset();
+        addEvent();
+    }
+    init();
 }
-// reset 함수를 통해 현재값을 2(3번째)를 기본으로 설정해주고
-function reset() {
-    cuId = 2;
-    // 이전값의 li에는 활성화 클래스를 제거해주고
-    listItemEls[exId].classList.remove('selected');
-    // 현재값의 li에는 활성화 클래스를 추가해주고
-    listItemEls[cuId].classList.add('selected');
-    // resizeGallery를 호출시킴으로써 원활하게 작동을 시킨 뒤
-    resizeGallery();
-    // 이전값에 현재값을 넣어 다음 작동 시에도 원활하게 진행되도록 설정해줍니다.
-    exId = cuId;
-}
-// 초기화 함수에서 리셋 후
-// 이벤트 기능이 실행되도록 호출해준 뒤
-function init() {
-    reset();
-    addEvent();
-}
-// 모든 순서 및 설계가 끝난 함수를 실행시켜줍니다.
-init();
