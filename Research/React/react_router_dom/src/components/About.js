23203f506f83d098af90306b1ead8ceb7b745f99
rlacodud <74367207+rlacodud@users.noreply.github.com>
2022-04-25 15:38:12 +0900

    React_Router_1차

--- Research/React/react_router_dom/src/components/About.js
+++ Research/React/react_router_dom/src/components/About.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
+import { useSearchParams } from 'react-router-dom';
 
-export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <h1>About</h1>
-        <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
-      </div>
-    )
-  }
-}
+const About = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get('detail');
+  const mode = searchParams.get('mode');
+
+  const onToggleDetail = () => {
+    setSearchParams({mode, detail: detail === 'true' ? false: true});
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParams({mode:nextMode, detail});
+  };
+
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>mode + 1</button>
+    </div>
+  );
+};
+
+export default About;
