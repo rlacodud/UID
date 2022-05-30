6c1e157e2f6c0e1cd6cdd8ea9e6953f06177644d
Sol <solpark16@naver.com>
2022-05-30 13:27:54 +0900

    section6 update

--- LGGram/js/script2.js
+++ LGGram/js/script2.js
@@ -22,11 +22,7 @@ function penScrollEvent() {
   console.log('fixedAreaBottom = '+ fixedAreaBottom);
   console.log('section7Top = '+ section7Top);
 
-  if (section6Top < documentHeight) {
-    $('.fixed-area').addClass('fixed');
-  } else {
-    $('.fixed-area').removeClass('fixed');
-  }
+
 
   // if (fixedAreaBottom >= section7Top) {
   //   console.log('ddd')
@@ -38,7 +34,6 @@ function penScrollEvent() {
   // }
 
   if (documentHeight > section7Top - windowHeight) {
-    console.log('그렇습니다');
     $('.fixed-area').removeClass('fixed');
     $('.fixed-area').addClass('absolute');
   } else if (documentHeight < section7Top - windowHeight && documentHeight >= section6Top){
@@ -58,20 +53,22 @@ gsap.timeline({scrollTrigger:{trigger:'.scrollDist4', start:'top bottom', end:'b
 .fromTo('.sub-area', {y: 0},{y: 0}, 0)
 
 .fromTo('.pen2', {scale: 0.15},{scale: 0.15}, 0) 
-.fromTo('.pen2', {y: -70},{y: -70}, 0)
-.fromTo('.pen2', {x: -120},{x: -120}, 0)
+.fromTo('.pen2', {y: -200},{y: -200}, 0)
+.fromTo('.pen2', {x: -200},{x: -200}, 0)
 .fromTo('.pen2', {rotation: -28},{rotation: -28}, 0)
 
 gsap.set('.scrollDist3', {width:'720px', height:'30%'})
 gsap.timeline({scrollTrigger:{trigger:'.scrollDist3', start:'top bottom', end:'bottom bottom', scrub:0}})
-.fromTo('.title-area', {y: 0},{y: -1000}, 0)
-.fromTo('.line-area', {y: 0},{y: -1000}, 0)
-.fromTo('.background-area', {y: 0},{y: -1000}, 0)
-.fromTo('.sub-area', {y: 1000},{y: 0}, 0)
+.fromTo('.title-area', {y: 0},{y: -1200}, 0)
+.fromTo('.line-area', {y: 0},{y: -1200}, 0)
+.fromTo('.background-area', {y: 0},{y: -1200}, 0)
+.fromTo('.sub-area', {y: 1200},{y: 0}, 0)
+
+
 
 .fromTo('.pen2', {scale: 1},{scale: 0.15}, 0) 
-.fromTo('.pen2', {y: 48},{y: -70}, 0)
-.fromTo('.pen2', {x: -50},{x: -120}, 0)
+.fromTo('.pen2', {y: 48},{y: -200}, 0)
+.fromTo('.pen2', {x: -50},{x: -200}, 0)
 .fromTo('.pen2', {rotation: -95},{rotation: -28}, 0)
 
 
