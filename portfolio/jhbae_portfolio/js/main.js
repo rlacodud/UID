a585de905f998c9767bfe7b5f89c28137dfb167b
bhyun511 <54615250+bhyun511@users.noreply.github.com>
2022-06-02 16:54:38 +0900

    폴더정리 (pf 폴더명 수정)

--- portfolio/jhbae_portfolio/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        anchors:['section1','section2','section3','section4','section5'],
-        autoScrolling:true,
-        scrollhorizontally: true,
-        navigation: true,
-        navigationPosition: 'right'
-    });
-});
